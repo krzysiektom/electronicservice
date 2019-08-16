fd3ad19b893dad4b7b233359abc26e9ad393e3a1
krzysztof <krzysiektom@op.pl>
2019-08-16 18:11:53 +0200

    Initial commit

---
+++ src/main/java/pl/tomala/electronicservice/ElectronicserviceApplication.java
@@ -0,0 +1,13 @@
+package pl.tomala.electronicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicserviceApplication.class, args);
+    }
+
+}
